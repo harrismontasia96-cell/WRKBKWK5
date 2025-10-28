ad57813c909959b105e5e5632892b005022eaf8f
montasia harris <mharris@my.yearupunited.com>
2025-10-28 10:37:31 -0400

    wrkbkwk5 Vehicle exercise

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,15 +3,15 @@ package com.pluralsight;
 public class Main {
 
     public static void main(String[] args) {
-        Moped slowRide = new Moped();
+        Moped slowRide = new Moped("Red",3,300,17,"Red");
         slowRide.setColor("Red");
         slowRide.setFuelCapacity(5);
 
-        Car Barbie  = new Car("pink",2, );
+        Car Barbie  = new Car("pink",2,200,16,true );
         Barbie.setColor("Pink");
         Barbie.setFuelCapacity(10);
 
-        SemiTruck  TransFormer = new SemiTruck();
+        SemiTruck  TransFormer = new SemiTruck("yellow");
         TransFormer.setColor("Optimis Prime");
         TransFormer.setFuelCapacity(100);
 
